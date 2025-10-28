ece3d09a106f64fdd3697e516f7f2cd9965358f2
Ydris <ydris.benmansour@studioblackflag.com>
2025-10-28 14:59:51 +0100

    add-gravity

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,9 +1,16 @@
 class_name Player extends CharacterBody2D
 
+@export_category("References")
+@export var ray_cast: RayCast2D
 
+@export_category("Player Movement")
 @export var max_speed: float = 500.0
 @export var acceleration: float = 1000.0
 @export var braking_strength: float = 3000.0
+
+@export_category("Gravity")
+@export var max_fall_speed: float = 2000.0
+@export var gravity: float = 4000.0
 
 var move_dir: Vector2 = Vector2.ZERO
 
@@ -21,6 +28,7 @@ func get_input():
 
 func move_player(delta: float):
 	handle_acceleration(delta)
+	handle_gravity(delta)
 	move_and_slide()
 
 
@@ -28,11 +36,24 @@ func handle_acceleration(delta: float):
 	var dir: float = 1.0 if velocity.x >= 0.0 else -1.0
 	if move_dir.length() != 0:
 		velocity.x += move_dir.x * acceleration * delta
-		if velocity.normalized().x != move_dir.normalized().x:
+		if (velocity.x * Vector2.RIGHT).normalized().x != (move_dir.x * Vector2.RIGHT).normalized().x:
 			velocity.x -= braking_strength * dir * delta
 		if velocity.x * dir > max_speed:
 			velocity.x = max_speed * dir
-	else:
-		velocity.x -= braking_strength * dir * delta
-		if velocity.x * dir < 0.0:
-			velocity.x = 0.0
+		return
+	
+	velocity.x -= braking_strength * dir * delta
+	if velocity.x * dir < 0.0:
+		velocity.x = 0.0
+
+
+func handle_gravity(delta: float):
+	if ray_cast.is_colliding():
+		if velocity.y < 0.0:
+			velocity.y -= braking_strength
+			if velocity.y > 0.0: velocity.y = 0.0
+		return
+	
+	velocity.y += gravity * delta
+	if velocity.y < -max_fall_speed:
+		velocity.y = -max_fall_speed
