547b1bd9b43700df1e98e7418f8138d59a56b82b
PeacefullSh33p <hugofustitiger@gmail.Com>
2025-10-28 16:21:17 +0100

    Suppression HugoTestScripts

--- Scripts/HugoTestScript/EnnemiTest.gd
+++
@@ -1,8 +0,0 @@
-class_name Ennemi
-extends "res://Scripts/HugoTestScript/Entity.gd"
-
-func HandleCollision(body: Node2D):
-	if body is Player:
-		var ennemi = body as Player
-		var DamageToTake = ennemi.Damage
-		ReceiveDamage(DamageToTake)
