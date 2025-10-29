8bbc658dc9032801731d57e24cefad047dae8b86
aznoqmous <paul.landreau931v@gmail.com>
2025-10-29 12:31:49 +0100

    added 10 worlds

--- Scripts/MainScene.gd
+++ Scripts/MainScene.gd
@@ -8,17 +8,17 @@ class_name MainScene extends Node2D
 @export var worlds : Array[PackedScene]
 var current_world_index = 0
 
-func load_next_worl():
+func load_next_world():
 	current_world_index += 1
 	load_world(worlds[current_world_index])
-	
+
 func load_world(world: PackedScene):
 	await game.scene_transition.show_transition()
 	var new_world := world.instantiate() as World
 	current_world.queue_free()
+	current_world = new_world
 	add_child(new_world)
 	player.set_visible(false)
-	current_world = new_world
 	player.global_position = new_world.player_spawn.global_position
 	player.set_visible(true)
 	game.scene_transition.hide_transition()
